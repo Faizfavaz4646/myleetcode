ac12bf5e74cfa6515cf15fc5dfd839fb52145687
Faizfavaz4646 <faizfavaz7@gmail.com>
2025-05-17 17:00:56 +0530

    Time: 0 ms (100%), Space: 53.1 MB (88.14%) - LeetHub

---
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.js
@@ -0,0 +1,10 @@
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+function strStr(haystack, needle) {
+    return index=haystack.indexOf(needle)
+    
+};
+console.log(strStr("sadbutsad"),"sad")
