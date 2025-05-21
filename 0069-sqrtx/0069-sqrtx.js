86ccbf5d55a247bf7ae5bca7d57ddf1e1c17c512
Faizfavaz4646 <faizfavaz7@gmail.com>
2025-05-21 09:57:12 +0530

    Time: 0 ms (100%), Space: 57.5 MB (7.13%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number} x
+ * @return {number}
+ */
+var mySqrt = function(x) {
+    let result=Math.sqrt(x);
+    return Math.floor(result)
+
+
+}
+console.log(mySqrt(8))
+  
+ 
+
+
