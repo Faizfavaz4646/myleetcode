363a15c4efe7749326a91ecfb84862797cddf569
Faizfavaz4646 <faizfavaz7@gmail.com>
2025-05-24 11:33:15 +0530

    Time: 0 ms (100%), Space: 57.5 MB (81.63%) - LeetHub

---
+++ 2058-concatenation-of-array/2058-concatenation-of-array.js
@@ -0,0 +1,10 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var getConcatenation = function(nums) {
+    let nums2=nums;
+   const join=nums.concat(nums2);
+   return join;
+    
+};
