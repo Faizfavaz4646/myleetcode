481fde0bddf8f504b86aaf80db4909c702ad904d
Faizfavaz4646 <faizfavaz7@gmail.com>
2025-05-29 15:00:59 +0530

    Time: 2 ms (74.86%), Space: 55.9 MB (64.4%) - LeetHub

--- 2219-maximum-number-of-words-found-in-sentences/2219-maximum-number-of-words-found-in-sentences.js
+++ 2219-maximum-number-of-words-found-in-sentences/2219-maximum-number-of-words-found-in-sentences.js
@@ -3,11 +3,11 @@
  * @return {number}
  */
 var mostWordsFound = function(sentences) {
-    let maxword=0
+    let maxword=0;
     for(sentence of sentences){
         const wordcount=sentence.split(" ").length
         if(wordcount > maxword){
-            maxword=wordcount
+            maxword=wordcount;
         }
       
     }
